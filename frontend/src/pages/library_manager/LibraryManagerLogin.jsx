0b9332606e1353553cd77611a7fbad180f4bb363
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-13 04:02:47 +0530

    Add LibraryManagerLogin component for library manager authentication

---
+++ frontend/src/pages/library_manager/LibraryManagerLogin.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { LoginPage } from "../../components/index";
+
+function LibraryManagerLogin() {
+  return (
+    <>
+      <LoginPage
+        loginSectionCSS="from-[#38BDF8] to-[#8B5CF6]"
+        buttonCSS="bg-[#6EE7B7] hover:bg-[#5CC198FF] hover:shadow-[4px_4px_16px_1px_rgba(7,59,76,0.4)]"
+        imageSrc="library_manager.png"
+        imageAlt="Library Manager login image"
+        h1CSS="text-[#1E3A8A]"
+        inputCSS="active:border-[#6EE7B7] focus:border-[#5CC198FF]"
+        aLinkCSS="text-[#FDE68A] hover:text-[#FFE26DFF] hover:text-shadow-[0_0_10px_rgba(250, 210, 50,0.5)]"
+        API="library_manager"
+      />
+    </>
+  );
+}
+
+export default LibraryManagerLogin;
