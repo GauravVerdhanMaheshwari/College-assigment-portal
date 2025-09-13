edfb6522fe90034cc967fa283a71bd128a497b9e
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-13 13:37:55 +0530

    Add UserManager and LibraryManager components to index export

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,10 +2,18 @@ import StudentLogin from "./student/StudentLogin";
 import StudentForgetPassword from "./student/StudentForgetPassword";
 import FacultyLogin from "./faculties/FacultiesLogin";
 import FacultyForgetPassword from "./faculties/FacultiesForgetPassword";
+import UserManagerLogin from "./user_manager/UserManagerLogin";
+import UserManagerForgetPassword from "./user_manager/UserManagerForgetPassword";
+import LibraryManagerLogin from "./library_manager/LibraryManagerLogin";
+import LibraryManagerForgetPassword from "./library_manager/LibraryManagerForgetPassword";
 
 export {
   StudentLogin,
   StudentForgetPassword,
   FacultyLogin,
   FacultyForgetPassword,
+  UserManagerLogin,
+  UserManagerForgetPassword,
+  LibraryManagerLogin,
+  LibraryManagerForgetPassword,
 };
