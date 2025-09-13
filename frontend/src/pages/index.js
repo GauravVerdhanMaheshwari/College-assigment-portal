c07c748689c1963a6202ddcc6f13e6bf39ece9d0
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-13 13:10:17 +0530

    Fix import statement for StudentLogin in index.js to ensure correct module export

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,11 @@
-import Login from "./student/StudentLogin";
+import StudentLogin from "./student/StudentLogin";
 import StudentForgetPassword from "./student/StudentForgetPassword";
 import FacultyLogin from "./faculties/FacultiesLogin";
 import FacultyForgetPassword from "./faculties/FacultiesForgetPassword";
 
-export { Login, StudentForgetPassword, FacultyLogin, FacultyForgetPassword };
+export {
+  StudentLogin,
+  StudentForgetPassword,
+  FacultyLogin,
+  FacultyForgetPassword,
+};
