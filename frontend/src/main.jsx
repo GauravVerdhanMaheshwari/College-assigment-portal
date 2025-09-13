21f97d7d7b5e81083ba52be3e25cf53f7abc1600
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-13 13:26:27 +0530

    Refactor main.jsx to implement routing with react-router and Layout component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {
+  StudentLogin,
+  FacultyLogin,
+  StudentForgetPassword,
+  FacultyForgetPassword,
+} from "./pages/index.js";
+import { createRoot } from "react-dom/client";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<StudentLogin />} />
+      <Route path="faculty-login" element={<FacultyLogin />} />
+      <Route
+        path="student-forget-password"
+        element={<StudentForgetPassword />}
+      />
+      <Route
+        path="faculty-forget-password"
+        element={<FacultyForgetPassword />}
+      />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <RouterProvider router={router} />
+  </>
+);
