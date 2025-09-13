c280bf5088a5ef9b2c7657f5fa5ff2234d8505ad
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-13 13:38:10 +0530

    Add text shadow to h1 in LibraryManagerLogin component

--- frontend/src/pages/library_manager/LibraryManagerLogin.jsx
+++ frontend/src/pages/library_manager/LibraryManagerLogin.jsx
@@ -9,10 +9,11 @@ function LibraryManagerLogin() {
         buttonCSS="bg-[#6EE7B7] hover:bg-[#5CC198FF] hover:shadow-[4px_4px_16px_1px_rgba(7,59,76,0.4)]"
         imageSrc="library_manager.png"
         imageAlt="Library Manager login image"
-        h1CSS="text-[#1E3A8A]"
+        h1CSS="text-[#1E3A8A] text-shadow-[0_0_10px_rgba(30,58,138,0.5)]"
         inputCSS="active:border-[#6EE7B7] focus:border-[#5CC198FF]"
         aLinkCSS="text-[#FDE68A] hover:text-[#FFE26DFF] hover:text-shadow-[0_0_10px_rgba(250, 210, 50,0.5)]"
         API="library_manager"
+        forgetPasswordLink="/library-manager-forget-password"
       />
     </>
   );
