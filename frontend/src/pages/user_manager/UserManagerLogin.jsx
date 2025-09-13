b1b10c9dc0f87c23a8f49020865d08602aed77e6
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-13 14:28:05 +0530

    Update styles and image source in UserManagerLogin component for consistency

--- frontend/src/pages/user_manager/UserManagerLogin.jsx
+++ frontend/src/pages/user_manager/UserManagerLogin.jsx
@@ -5,11 +5,13 @@ function UserManagerLogin() {
     <LoginPage
       inputCSS="active:border-[#0EA5E9] focus:border-[#1398C4FF]"
       loginSectionCSS="from-[#C4B5FD] to-[#C4B5FD]"
-      aLinkCSS="text-[#C4B5FD] hover:text-[#C4B5FD] hover:text-shadow-[0_0_10px_rgba(245, 243, 255, 0.5)]"
+      aLinkCSS="text-[#0A9FE0FF] hover:text-[#0A9FE0FF] hover:text-shadow-[0_0_10px_rgba(10,159,224,0.5)]"
       buttonCSS="bg-[#38BDF8] hover:bg-[#0A9FE0FF] hover:shadow-[4px_4px_16px_1px_rgba(15,121,156,0.4)]"
-      imageSrc="userManager.png"
+      imageSrc="user_manager.png"
       imageAlt="User Manager login image"
       API="user_manager"
+      h1CSS="text-[#0EA5E9] text-shadow-[0_0_10px_rgba(14,165,233,0.5)]"
+      forgetPasswordLink="/user-manager-forget-password"
     />
   );
 }
