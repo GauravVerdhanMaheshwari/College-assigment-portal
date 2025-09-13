84942dfb4b7c4efaf71f5198bc1595b60351160a
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-13 11:51:13 +0530

    Enhance aLinkCSS in StudentLogin component for improved hover effect

--- frontend/src/pages/student/StudentLogin.jsx
+++ frontend/src/pages/student/StudentLogin.jsx
@@ -5,7 +5,7 @@ function StudentLogin() {
     <LoginPage
       inputCSS="active:border-[#118AB2] focus:border-[#118AB2]"
       loginSectionCSS="from-[#FFE9B5] to-[#FFD166]"
-      aLinkCSS="text-[#EF476F] hover:text-[#d63c57]"
+      aLinkCSS="text-[#EF476F] hover:text-[#d63c57] hover:text-shadow-[0_0_10px_rgba(239,71,111,0.5)]"
       buttonCSS="bg-[#118AB2] hover:bg-[#0f799c] hover:shadow-[4px_4px_16px_1px_rgba(15,121,156,0.4)]"
       imageSrc="student.png"
       imageAlt="Student login image"
