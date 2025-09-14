e3e52433fd0e3c13f6654c511b472a6e4996936f
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-14 10:22:00 +0530

    feat: add student model with schema definition

---
+++ backend/Models/student.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  enrollmentNumber: { type: Number, required: true, unique: true },
+  course: { type: String, required: true },
+  division: { type: String, required: true },
+  year: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("Student", studentSchema);
