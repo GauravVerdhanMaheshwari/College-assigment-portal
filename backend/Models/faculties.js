270ba061cbcc8fd1160a391fd69167fc0da8b747
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-14 11:51:57 +0530

    feat: add faculty model with schema definition

---
+++ backend/Models/faculties.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const facultySchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  course: { type: String, required: true },
+  division: { type: String, required: true },
+  subject: { type: String, required: true },
+  year: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("Faculty", facultySchema);
