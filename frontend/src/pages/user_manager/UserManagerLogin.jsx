cd50c4c73e61ff5a40e5e5107a2e1cb23fed6da3
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-16 00:27:41 +0530

    fix: correct gradient colors in UserManagerLogin component

--- frontend/src/pages/user_manager/UserManagerLogin.jsx
+++ frontend/src/pages/user_manager/UserManagerLogin.jsx
@@ -4,7 +4,7 @@ function UserManagerLogin() {
   return (
     <LoginPage
       inputCSS="active:border-[#0EA5E9] focus:border-[#1398C4FF]"
-      loginSectionCSS="from-[#C4B5FD] to-[#8B5CF6]"
+      loginSectionCSS="from-[#C4B5FD] to-[#C4B5FD]"
       aLinkCSS="text-[#0A9FE0FF] hover:text-[#0A9FE0FF] hover:text-shadow-[0_0_10px_rgba(10,159,224,0.5)]"
       buttonCSS="bg-[#38BDF8] hover:bg-[#0A9FE0FF] hover:shadow-[4px_4px_16px_1px_rgba(15,121,156,0.4)]"
       imageSrc="user_manager.png"
