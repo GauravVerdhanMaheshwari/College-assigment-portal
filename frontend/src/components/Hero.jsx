127a7695d384a252626a399b116f731b03822b7a
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-15 23:19:49 +0530

    feat: implement Hero component with dynamic greeting and background

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Hero({ heroBgColor, heroImg }) {
+  const date = new Date();
+  const hours = date.getHours();
+  let greeting;
+  if (hours < 12) {
+    greeting = "Good Morning";
+  } else if (hours < 18) {
+    greeting = "Good Afternoon";
+  } else {
+    greeting = "Good Evening";
+  }
+  const time = date.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const currentDate = date.toLocaleDateString();
+
+  return (
+    <div
+      className={`flex flex-row gap-4 flex-grow items-center justify-center h-full mx-20 mt-2 bg-gradient-to-t ${heroBgColor} border border-gray-300 rounded-3xl py-10 px-30 shadow-lg`}
+    >
+      <div className="flex flex-col flex-1 items-start ml-10">
+        <p className="text-4xl text-white mb-3 text-shadow-[2px_2px_10px_rgba(255,255,255,0.5)] font-bold">
+          WELCOME BACK !
+        </p>
+        <p className="text-3xl text-white text-shadow-[2px_2px_10px_rgba(255,255,255,0.5)]">
+          {greeting}, it's {time} on {currentDate}.
+        </p>
+      </div>
+      <div className="flex flex-1 flex-col mr-10 items-end">
+        <div className="mb-4 w-100 h-56">
+          <img
+            src={heroImg}
+            alt={`Image of ${heroImg}`}
+            className="rounded-lg w-full h-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
