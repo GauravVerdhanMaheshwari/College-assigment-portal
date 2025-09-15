f5f2c0825c80e5d258d66359dd2de753dd479322
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-15 18:55:35 +0530

    fix: standardize h1CSS prop syntax in StudentLogin component

--- frontend/src/pages/student/StudentLogin.jsx
+++ frontend/src/pages/student/StudentLogin.jsx
@@ -4,7 +4,7 @@ function StudentLogin() {
   return (
     <LoginPage
       inputCSS="active:border-[#118AB2] focus:border-[#118AB2]"
-      h1CSS={"text-[#073B4C] text-shadow-[2px_2px_5px_rgba(7,59,76,0.5)]"}
+      h1CSS="text-[#073B4C] text-shadow-[2px_2px_5px_rgba(7,59,76,0.5)]"
       loginSectionCSS="from-[#FFE9B5] to-[#FFD166]"
       aLinkCSS="text-[#EF476F] hover:text-[#d63c57] hover:text-shadow-[0_0_10px_rgba(239,71,111,0.5)]"
       buttonCSS="bg-[#118AB2] hover:bg-[#0f799c] hover:shadow-[4px_4px_16px_1px_rgba(15,121,156,0.4)]"
@@ -12,6 +12,7 @@ function StudentLogin() {
       imageAlt="Student login image"
       API="students"
       forgetPasswordLink="/student-forget-password"
+      redirectLink="/student"
       pageAbout="Student"
     />
   );
