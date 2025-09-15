a20d8c04b5520295b340009b3fe0d7fe0da091b2
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-16 00:53:28 +0530

    feat: enhance UserManagerHomePage layout and integrate List component for dynamic user display

--- frontend/src/pages/user_manager/UserManagerHomePage.jsx
+++ frontend/src/pages/user_manager/UserManagerHomePage.jsx
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 function UserManagerHomePage() {
   const navigate = useNavigate();
   const user = JSON.parse(sessionStorage.getItem("user"));
-  console.log(user);
 
   if (!user) {
     navigate("/user-manager-login");
@@ -15,21 +14,32 @@ function UserManagerHomePage() {
   return (
     <div className="w-full h-full bg-gradient-to-b from-[#C4B5FD] to-[#8B5CF6]">
       <div className="flex flex-col h-full">
-        <div>
+        <div className="mb-6">
           <Header
             textColor="text-[#0EA5E9]"
             headerStyle="to-[#C4B5FD] from-[#A37BFFFF]"
           />
         </div>
-        <div className="flex flex-col p-0 mt-5">
+
+        <div className="mb-10">
           <Hero
             heroImg="user_manager_hero.jpg"
             heroBgColor="to-[#A37BFFFF] from-[#C4B5FD]"
           />
         </div>
-        <div className="flex flex-col p-0 mt-5">
-          <List />
-        </div>
+
+        <List
+          entityNames={["Students", "Faculties"]}
+          entityFields={[
+            ["Enrollment No", "Name", "Email", "Course", "Division", "Year"],
+            ["Name", "Email", "Subject", "Course", "Year", "Division"],
+          ]}
+          entityKeys={[
+            ["enrollmentNumber", "name", "email", "course", "division", "year"],
+            ["name", "email", "subject", "course", "year", "division"],
+          ]}
+          entityEndpoints={["students", "faculties"]}
+        />
       </div>
     </div>
   );
