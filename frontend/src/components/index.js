7302c52fc817770a15cc31a141db4ef5a21ced11
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-16 00:42:10 +0530

    feat: export List component in index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,5 +2,6 @@ import LoginPage from "./LoginPage.jsx";
 import PasswordPage from "./PasswordPage.jsx";
 import Header from "./Header.jsx";
 import Hero from "./Hero.jsx";
+import List from "./List.jsx";
 
-export { LoginPage, PasswordPage, Header, Hero };
+export { LoginPage, PasswordPage, Header, Hero, List };
