3f5e950f59685e717682a2f565658a7750a02df0
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-16 16:53:26 +0530

    feat: export Filter and Search components in index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,5 +3,7 @@ import PasswordPage from "./PasswordPage.jsx";
 import Header from "./Header.jsx";
 import Hero from "./Hero.jsx";
 import List from "./List.jsx";
+import Filter from "./Filter.jsx";
+import Search from "./Search.jsx";
 
-export { LoginPage, PasswordPage, Header, Hero, List };
+export { LoginPage, PasswordPage, Header, Hero, List, Filter, Search };
