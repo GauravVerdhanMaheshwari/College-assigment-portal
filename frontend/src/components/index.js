ed26e8fcf9145de35269fdba9d1f7b3cd9f97b76
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-16 20:49:25 +0530

    feat: export AddUsers component in index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -5,5 +5,15 @@ import Hero from "./Hero.jsx";
 import List from "./List.jsx";
 import Filter from "./Filter.jsx";
 import Search from "./Search.jsx";
+import AddUsers from "./AddUsers.jsx";
 
-export { LoginPage, PasswordPage, Header, Hero, List, Filter, Search };
+export {
+  LoginPage,
+  PasswordPage,
+  Header,
+  Hero,
+  List,
+  Filter,
+  Search,
+  AddUsers,
+};
