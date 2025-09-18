cd8cc306c0c145f90f5929b8d5b438285d84db5d
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-18 13:33:51 +0530

    feat: create index file for student page components

---
+++ frontend/src/pages/student/index.js
@@ -0,0 +1,11 @@
+import StudentAssignmentsList from "./StudentAssignmentsList.jsx";
+import PeerAssignments from "./PeerAssignments.jsx";
+import AssignmentUploadForm from "./AssignmentUploadForm.jsx";
+import FutureAssignmentsList from "./FutureAssignmentsList.jsx";
+
+export {
+  StudentAssignmentsList,
+  PeerAssignments,
+  AssignmentUploadForm,
+  FutureAssignmentsList,
+};
