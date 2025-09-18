c1fd2a58c959f0dd443a951bd30043f62e308c4f
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-09-18 13:35:02 +0530

    feat: remove PeerAssignments import and export from student index file

--- frontend/src/pages/student/index.js
+++ frontend/src/pages/student/index.js
@@ -1,11 +1,5 @@
 import StudentAssignmentsList from "./StudentAssignmentsList.jsx";
-import PeerAssignments from "./PeerAssignments.jsx";
 import AssignmentUploadForm from "./AssignmentUploadForm.jsx";
 import FutureAssignmentsList from "./FutureAssignmentsList.jsx";
 
-export {
-  StudentAssignmentsList,
-  PeerAssignments,
-  AssignmentUploadForm,
-  FutureAssignmentsList,
-};
+export { StudentAssignmentsList, AssignmentUploadForm, FutureAssignmentsList };
