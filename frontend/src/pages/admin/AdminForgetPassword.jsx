b63b32e7279d2d0f509a7ea89eb85ddfd60f1628
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-10-23 03:04:07 +0530

    fix: correct image source path and update login page link in AdminForgetPassword component

--- frontend/src/pages/admin/AdminForgetPassword.jsx
+++ frontend/src/pages/admin/AdminForgetPassword.jsx
@@ -6,13 +6,13 @@ function AdminForgetPassword() {
     <PasswordPage
       loginSectionCSS="from-[#A7F3D0] to-[#34D399]"
       buttonCSS="bg-[#3B96FFFF] hover:bg-[#55A3FCFF] hover:shadow-[4px_4px_16px_1px_rgba(7,59,76,0.4)]"
-      imageSrc="admin.png"
+      imageSrc="../../../public/admin.png"
       imageAlt="Admin login image"
       h1CSS="text-[#4D7BFAFF] text-shadow-[0_0_10px_rgba(30,58,138,0.5)]"
       inputCSS="active:border-[#3B96FFFF] focus:border-[#55A3FCFF]"
       aLinkCSS="text-[#3B96FFFF] hover:text-[#55A3FCFF] hover:text-shadow-[0_0_10px_rgba(59,150,255,0.5)]"
       API="admin"
-      loginPageLink="/admin-login"
+      loginPageLink="/admin/login"
     />
   );
 }
