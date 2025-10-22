9b922d84c8b424ae0094b0defe4068bea7a07c1b
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-10-23 04:23:02 +0530

    fix: correct image source path in StudentLogin component

--- frontend/src/pages/student/StudentLogin.jsx
+++ frontend/src/pages/student/StudentLogin.jsx
@@ -8,7 +8,7 @@ function StudentLogin() {
       loginSectionCSS="from-[#FFE9B5] to-[#FFD166]"
       aLinkCSS="text-[#EF476F] hover:text-[#d63c57] hover:text-shadow-[0_0_10px_rgba(239,71,111,0.5)]"
       buttonCSS="bg-[#118AB2] hover:bg-[#0f799c] hover:shadow-[4px_4px_16px_1px_rgba(15,121,156,0.4)]"
-      imageSrc="student.png"
+      imageSrc="./student.png"
       imageAlt="Student login image"
       API="students"
       forgetPasswordLink="/student-forget-password"
