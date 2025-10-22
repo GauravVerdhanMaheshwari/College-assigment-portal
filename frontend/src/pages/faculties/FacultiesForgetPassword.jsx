a219dbeae980b4d8bba2809ab77e311283724348
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-10-23 04:01:51 +0530

    fix: correct image source path and update login page link in FacultiesForgetPassword component

--- frontend/src/pages/faculties/FacultiesForgetPassword.jsx
+++ frontend/src/pages/faculties/FacultiesForgetPassword.jsx
@@ -9,9 +9,9 @@ function FacultiesForgetPassword() {
       h1CSS="text-[#4C1D95] text-shadow-[0px_0px_10px_rgba(124,58,237,0.9)]"
       aLinkCSS="text-[#95D6F4FF] hover:text-shadow-[0px_0_10px_rgba(62,191,250,0.9)]"
       buttonCSS="bg-[#4C1D95] hover:bg-[#4C1D95]/70 hover:shadow-[4px_4px_16px_1px_rgba(97,41,186,0.4)]"
-      imageSrc="faculty.png"
+      imageSrc="../../../public/faculty.png"
       imageAlt="Faculty login image"
-      loginPageLink="/faculties-login"
+      loginPageLink="/faculties/login"
       API={"faculty"}
     />
   );
