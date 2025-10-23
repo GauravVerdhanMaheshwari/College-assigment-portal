9081263df523ed36106a9b973e7c10dfe7208b71
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-10-23 15:31:33 +0530

    fix: update image source path in UserManagerForgetPassword component

--- frontend/src/pages/user_manager/UserManagerForgetPassword.jsx
+++ frontend/src/pages/user_manager/UserManagerForgetPassword.jsx
@@ -8,7 +8,7 @@ function UserManagerForgetPassword() {
       loginSectionCSS="from-[#C4B5FD] to-[#C4B5FD]"
       aLinkCSS="text-[#0A9FE0FF] hover:text-[#0A9FE0FF] hover:text-shadow-[0_0_10px_rgba(10,159,224,0.5)]"
       buttonCSS="bg-[#38BDF8] hover:bg-[#0A9FE0FF] hover:shadow-[4px_4px_16px_1px_rgba(15,121,156,0.4)]"
-      imageSrc="user_manager.png"
+      imageSrc="../../../public/user_manager.png"
       imageAlt="User Manager login image"
       API="user_manager"
       h1CSS="text-[#0EA5E9] text-shadow-[0_0_10px_rgba(14,165,233,0.5)]"
