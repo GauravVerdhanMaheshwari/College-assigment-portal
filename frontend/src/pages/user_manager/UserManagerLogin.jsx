8387e7737d79ae3d11f6767bb7476d3add2efbf7
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-10-23 15:31:43 +0530

    fix: update image source path and redirect link in UserManagerLogin component

--- frontend/src/pages/user_manager/UserManagerLogin.jsx
+++ frontend/src/pages/user_manager/UserManagerLogin.jsx
@@ -7,13 +7,13 @@ function UserManagerLogin() {
       loginSectionCSS="from-[#C4B5FD] to-[#C4B5FD]"
       aLinkCSS="text-[#0A9FE0FF] hover:text-[#0A9FE0FF] hover:text-shadow-[0_0_10px_rgba(10,159,224,0.5)]"
       buttonCSS="bg-[#38BDF8] hover:bg-[#0A9FE0FF] hover:shadow-[4px_4px_16px_1px_rgba(15,121,156,0.4)]"
-      imageSrc="user_manager.png"
+      imageSrc="../../../public/user_manager.png"
       imageAlt="User Manager login image"
       API="user-managers"
       h1CSS="text-[#0EA5E9] text-shadow-[0_0_10px_rgba(14,165,233,0.5)]"
       forgetPasswordLink="/userManager/forgetPassword"
       pageAbout="User Manager"
-      redirectLink="/userManager/home"
+      redirectLink="/userManager"
     />
   );
 }
