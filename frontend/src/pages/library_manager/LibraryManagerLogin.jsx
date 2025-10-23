c480d958d00a5d3deef7754e6fa221c31bf1b923
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-10-23 15:01:41 +0530

    fix: update image source path in LibraryManagerLogin component

--- frontend/src/pages/library_manager/LibraryManagerLogin.jsx
+++ frontend/src/pages/library_manager/LibraryManagerLogin.jsx
@@ -7,7 +7,7 @@ function LibraryManagerLogin() {
       <LoginPage
         loginSectionCSS="from-[#38BDF8] to-[#8B5CF6]"
         buttonCSS="bg-[#6EE7B7] hover:bg-[#5CC198FF] hover:shadow-[4px_4px_16px_1px_rgba(7,59,76,0.4)]"
-        imageSrc="library_manager.png"
+        imageSrc="../../../public/library_manager.png"
         imageAlt="Library Manager login image"
         h1CSS="text-[#1E3A8A] text-shadow-[0_0_10px_rgba(30,58,138,0.5)]"
         inputCSS="active:border-[#6EE7B7] focus:border-[#5CC198FF]"
