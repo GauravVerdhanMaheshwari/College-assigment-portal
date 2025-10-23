42ab30bf82ff0b0a05ebda2de2bf97f087aa0f29
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-10-23 15:17:41 +0530

    fix: update profile navigation path in StudentHomePage component

--- frontend/src/pages/student/StudentHomePage.jsx
+++ frontend/src/pages/student/StudentHomePage.jsx
@@ -125,7 +125,7 @@ function StudentHomePage() {
         searchPlaceholder="Search assignments..."
         textColor="text-[#073B4C] text-shadow-[0px_0px_10px_rgba(7,59,76,0.9)]"
         headerStyle="to-[#FFE9B5] from-[#FFD166]"
-        profileNavigate="/student-profile"
+        profileNavigate="/student/profile"
         dummyReports={requests}
         loginPage="/"
         menuLinks={[
