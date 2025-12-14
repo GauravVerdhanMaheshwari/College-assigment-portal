17b31c667ffec4bbf04f5c9922c645f1c2f5f953
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-12-14 22:50:02 +0530

    refactor: reorganize multer setup in paperRoute.js for clarity

--- backend/Routers/paperRoute.js
+++ backend/Routers/paperRoute.js
@@ -1,18 +1,15 @@
 const express = require("express");
-const router = express.Router();
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
 const paperController = require("../Controllers/paperController");
 
-// Create a new paper
+const router = express.Router();
+const upload = multer({ dest: "uploads/" });
+
 router.post("/upload", upload.single("file"), paperController.uploadPaperFile);
-// Get all papers
+
 router.get("/", paperController.getAllPapers);
-// Get a paper by ID
 router.get("/:id", paperController.getPaperById);
-// Update a paper by ID
 router.put("/:id", paperController.updatePaperById);
-// Delete a paper by ID
 router.delete("/:id", paperController.deletePaperById);
 
 module.exports = router;
