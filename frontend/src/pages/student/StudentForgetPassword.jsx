628513e211cdb837d33ccf4b06413290712de6f7
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-12-14 19:23:37 +0530

    fix: correct image source and update login page link in StudentForgetPassword component

--- frontend/src/pages/student/StudentForgetPassword.jsx
+++ frontend/src/pages/student/StudentForgetPassword.jsx
@@ -6,13 +6,13 @@ function StudentForgetPassword() {
     <PasswordPage
       loginSectionCSS="from-[#FFE9B5] to-[#FFD166]"
       buttonCSS="bg-[#118AB2] hover:bg-[#0f799c] hover:shadow-[4px_4px_16px_1px_rgba(15,121,156,0.4)]"
-      imageSrc="student.png"
+      imageSrc="/student.png"
       imageAlt="Student login image"
       h1CSS="text-[#073B4C]"
       inputCSS="active:border-[#118AB2] focus:border-[#118AB2]"
       aLinkCSS="text-[#EF476F] hover:text-[#ec3762] hover:text-shadow-[0_0_10px_rgba(239,71,111,0.5)]"
       API="student"
-      loginPageLink="/"
+      loginPageLink="/student/login"
     />
   );
 }
