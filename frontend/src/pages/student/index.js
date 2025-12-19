239754e12ba6cb01bb333baadd636219c35b6bb0
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-12-20 00:34:00 +0530

    feat: export OtherStudentPaper component in student index file

--- frontend/src/pages/student/index.js
+++ frontend/src/pages/student/index.js
@@ -2,3 +2,4 @@ export { default as StudentAssignmentsList } from "./StudentAssignmentsList";
 export { default as FutureAssignmentsList } from "./FutureAssignmentsList";
 export { default as AssignmentUploadForm } from "./AssignmentUploadForm";
 export { default as StudentProfile } from "./StudentProfile";
+export { default as OtherStudentPaper } from "./OtherStudentPaper";
