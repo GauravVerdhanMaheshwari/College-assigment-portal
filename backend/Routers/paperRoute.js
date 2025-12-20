49c60e07a924ddb807b565d2c0f9ab337afd43b7
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-12-20 20:39:25 +0530

    feat: add userManager route for downloading papers

--- backend/Routers/paperRoute.js
+++ backend/Routers/paperRoute.js
@@ -9,9 +9,14 @@ router.post("/upload", upload.single("file"), paperController.uploadPaperFile);
 
 router.get("/:id/downloads", paperController.getDownloadHistory);
 router.get("/:id/download", paperController.downloadPaper);
+router.get(
+  "/:paperId/download/userManager",
+  paperController.downloadPaperForUserManager
+);
 
 router.get("/student/:studentId", paperController.getPapersByStudentId);
 router.get("/faculty", paperController.getFacultyPapers);
+router.get("/userManager", paperController.getUserManagerPapers);
 
 router.patch("/:id/toggle-visibility", paperController.toggleVisibility);
 router.patch("/:id/grade", paperController.gradePaper);
