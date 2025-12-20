5a0f1390eb7001fa849cb9d54a4345610e92adfc
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-12-20 16:59:07 +0530

    feat: change HTTP methods for access control routes from POST to PUT

--- backend/Routers/access_controlRoute.js
+++ backend/Routers/access_controlRoute.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const accessControlController = require("../Controllers/access_controlController");
 
-router.post("/grant", accessControlController.grantAccess);
-router.post("/revoke", accessControlController.revokeAccess);
-router.get("/check/:paperId/:userId", accessControlController.checkAccess);
+router.put("/grant", accessControlController.grantAccess);
+router.put("/revoke", accessControlController.revokeAccess);
+router.put("/request", accessControlController.requestAccess);
+router.get("/check/:paperId/:requestedBy", accessControlController.checkAccess);
 router.get("/accessible/:userId", accessControlController.getAccessiblePapers);
+
+// router.get("/requests/:ownerId", accessControlController.getAccessRequests);
 
 module.exports = router;
